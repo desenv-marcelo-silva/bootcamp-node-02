0ee6ebd86f0490391a4e085b2c7479c69754171f
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2020-02-03 16:56:33 -0300

    Configurações do Sequelize, estrutura de pastas e configurações do eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,26 @@
 module.exports = {
   env: {
-    commonjs: true,
     es6: true,
     node: true,
   },
   extends: [
     'airbnb-base',
+    'prettier'
   ],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
+    sourceType: 'module',
   },
   rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off", // por conta do Sequelize
+    "camelcase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }], // por conta dos middlewares
   },
 };
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "ufastfeet",
+  password: "pfastfeet",
+  database: "fastfeet",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
