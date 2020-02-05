4f559670b77a495dc3015134712bf3f3c2f451fb
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-02-05 08:11:11 -0300

    Seed para mudar valor apenas para admin.

---
+++ src/database/seeds/20200205105738-update-admin-field.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: QueryInterface => {
+    return QueryInterface.bulkUpdate('users', {
+      is_admin: true,
+    }, {
+      email: 'admin@fastfeet.com', 
+    },
+  );
+  },
+
+  down: () => {},
+};
