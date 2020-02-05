11cb226ef78d3991a7ee776ba2446621833c6007
Marcelo Lopes da Silva <desenv.marcelo.silva@gmail.com>
2020-02-05 09:11:25 -0300

    Inserir validação de destinatários.

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,9 +1,26 @@
 import Recipient from '../models/Recipient';
 
+import * as Yup from 'yup';
+
 import * as Error from '../util/Error';
+const CEPRegex = /^[0-9]{2}.[0-9]{3}-[0-9]{3}$/;
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      rua: Yup.string().required(),
+      numero: Yup.string().required(),
+      bairro: Yup.string().required(),
+      cidade: Yup.string().required(),
+      estado: Yup.string().required(),
+      cep: Yup.string().matches(CEPRegex),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return Error.BadRequest(res, 'Dados inválidos.' );
+    }
+
     const { name } = req.body;
     const recipientExists = await Recipient.findOne({ where: { name } });
     if (recipientExists) {
@@ -15,6 +32,21 @@ class RecipientController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      id: Yup.number().required(),
+      name: Yup.string().required(),
+      rua: Yup.string().required(),
+      numero: Yup.string().required(),
+      bairro: Yup.string().required(),
+      cidade: Yup.string().required(),
+      estado: Yup.string().required(),
+      cep: Yup.string().matches(CEPRegex),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return Error.BadRequest(res, 'Dados inválidos.' );
+    }
+
     const recipient = await Recipient.findByPk(req.body.id);
     if (!recipient) {
       return Error.BadRequest(res, 'Destinatário não está cadastrado.');
